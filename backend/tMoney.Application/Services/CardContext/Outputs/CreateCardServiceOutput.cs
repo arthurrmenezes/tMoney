48a1adb65df23adcc7c37bc483579c72d3baff69
arthurrmenezes <arthur.menezes2003@hotmail.com>
2026-01-18 15:16:12 -0300

    feat(card): Adicionar criação de cartão

---
+++ backend/tMoney.Application/Services/CardContext/Outputs/CreateCardServiceOutput.cs
@@ -0,0 +1,43 @@
+﻿namespace tMoney.Application.Services.CardContext.Outputs;
+
+public sealed class CreateCardServiceOutput
+{
+    public string Id { get; }
+    public string AccountId { get; }
+    public string Name { get; }
+    public CreateCardServiceOutputCreditCard? CreditCard { get; }
+    public DateTime? UpdatedAt { get; }
+    public DateTime CreatedAt { get; }
+
+    private CreateCardServiceOutput(string id, string accountId, string name, CreateCardServiceOutputCreditCard? creditCard, DateTime? updatedAt, 
+        DateTime createdAt)
+    {
+        Id = id;
+        AccountId = accountId;
+        Name = name;
+        CreditCard = creditCard;
+        UpdatedAt = updatedAt;
+        CreatedAt = createdAt;
+    }
+
+    public static CreateCardServiceOutput Factory(string id, string accountId, string name, CreateCardServiceOutputCreditCard? creditCard, DateTime? updatedAt,
+        DateTime createdAt)
+        => new(id, accountId, name, creditCard, updatedAt, createdAt);
+}
+
+public sealed class CreateCardServiceOutputCreditCard
+{
+    public decimal Limit { get; }
+    public int CloseDay { get; }
+    public int DueDay { get; }
+
+    private CreateCardServiceOutputCreditCard(decimal limit, int closeDay, int dueDay)
+    {
+        Limit = limit;
+        CloseDay = closeDay;
+        DueDay = dueDay;
+    }
+
+    public static CreateCardServiceOutputCreditCard Factory(decimal limit, int closeDay, int dueDay)
+        => new(limit, closeDay, dueDay);
+}
---
+++ backend/tMoney.WebApi/Controllers/CardContext/CardController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using tMoney.Application.Services.CardContext.Inputs;
+using tMoney.Application.Services.CardContext.Interfaces;
+using tMoney.Domain.ValueObjects;
+using tMoney.WebApi.Controllers.CardContext.Payloads;
+using tMoney.WebApi.Extensions;
+
+namespace tMoney.WebApi.Controllers.CardContext;
+
+[ApiController]
+[Route("api/v1/cards")]
+public class CardController : ControllerBase
+{
+    private readonly ICardService _cardService;
+
+    public CardController(ICardService cardService)
+    {
+        _cardService = cardService;
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> CreateCardAsync(
+        [FromBody] CreateCardPayload input,
+        CancellationToken cancellationToken)
+    {
+        var accountId = User.GetAccountId();
+
+        var creditCard = input.CreditCard is null ? null :
+            CreateCardServiceInputCreditCard.Factory(
+                limit: input.CreditCard.Limit,
+                closeDay: input.CreditCard.CloseDay,
+                dueDay: input.CreditCard.DueDay);
+
+        var serviceResult = await _cardService.CreateCardServiceAsync(
+            accountId: IdValueObject.Factory(accountId),
+            input: CreateCardServiceInput.Factory(
+                name: input.Name,
+                creditCard: creditCard),
+            cancellationToken: cancellationToken);
+
+        return Ok(serviceResult);
+        //return CreatedAtAction("GetCardById", new { cardId = serviceResult.Id }, serviceResult);
+    }
+}
